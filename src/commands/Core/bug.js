3508b1cd5e7f43f222e4e406e5c398116ae3fe4f
Louiswingu <louis.leroux.297@gmail.com>
2026-05-14 12:24:27 +0200

    Update bug.js

--- src/commands/Core/bug.js
+++ src/commands/Core/bug.js
@@ -9,20 +9,20 @@ export default {
 
     async execute(interaction) {
         const githubButton = new ButtonBuilder()
-            .setLabel('?? Report Bug on GitHub')
+            .setLabel('Report Bug on Discord')
             .setStyle(ButtonStyle.Link)
-            .setURL('https://github.com/codebymitch/TitanBot/issues');
+            .setURL('https://discord.gg/dvrmcw7H2d');
 
         const row = new ActionRowBuilder().addComponents(githubButton);
 
         const bugReportEmbed = createEmbed({
-            title: '?? Bug Report',
-            description: 'Found a bug? Please report it on our GitHub Issues page!\n\n' +
+            title: 'Bug Report',
+            description: 'Found a bug ? Please report it on our Discord Issues page!\n\n' +
             '**When reporting a bug, please include:**\n' +
-            '• ?? Detailed description of the issue\n' +
-            '• ?? Steps to reproduce the problem\n' +
-            '• ?? Screenshots if applicable\n' +
-            '• ?? Your bot version and environment\n\n' +
+            '?? Detailed description of the issue\n' +
+            '?? Steps to reproduce the problem\n' +
+            '?? Screenshots if applicable\n' +
+            '?? Your bot version and environment\n\n' +
             'This helps us fix issues faster and more effectively!',
             color: 'error'
         })
