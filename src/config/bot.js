9a643ea727d4032fa46001b7ed91c7a88adc301d
Louiswingu <louis.leroux.297@gmail.com>
2026-05-14 15:03:51 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Watching /help | Titan Bot").
-        name: "/help | EclipseX",
+        name: "/help",
         // Activity type number (3 = Watching).
         type: 0, 
       },
